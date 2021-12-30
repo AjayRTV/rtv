e5285e9410276318e2e222503b5c06edb15eb792
Asha <ajay.rewaatechverge@gmail.com>
2021-12-30 16:52:14 +0530

    todays commit

--- app/Http/Controllers/UserRoleController.php
+++ app/Http/Controllers/UserRoleController.php
@@ -15,62 +15,66 @@ use DataTables;
 use Exception;
 use App\role;
 
-class UserRoleController extends Controller {
-
-    // = -------------- [ ' Show Data into UserTable ' ] -------------- -=
-
-    public function getUserRole( Request $request, User $user ) {
-
-        $data = DB::table( 'userrole' )->get();
-
-        return response()->json( [ 'data' => $data ] );
-    }
-
+class UserRoleController extends Controller{
     // --------------------- [ Role user Add ] ---------------------
-
-    public function userRoleAdd( Request $request ) {
+    public function userRoleAdd(Request $request){
         try {
-            $duplicatemail = DB::table( 'userrole' )->where( 'email', $request->email )->get();
-
-            if ( count( $duplicatemail ) == 0 ) {
-                $addRoleData = DB::select( "INSERT INTO userrole(fname, lname,contact,email,password,role )VALUES('$request->firstname','$request->lastusername','$request->contact','$request->email','$request->password','$request->userrole')" );
-                return response()->json( [ 'addRoleData' => $addRoleData ], 200 );
+            $duplicatemail = DB::table('userrole')->where('email', $request->email)->get();
+            if (count($duplicatemail) == 0) {
+                $addRoleData = DB::select("INSERT INTO userrole(fname, lname,contact,email,password,role )VALUES('$request->firstname','$request->lastusername','$request->contact','$request->email','$request->password','$request->userrole')");
+                return response()->json(['addRoleData' => $addRoleData]);
             }
-        } catch( Exception $e ) {
-            return response()->json( 'faild', 200 );
+        } catch (Exception $e) {
+            return response()->json('faild');
         }
     }
-
     // *--------------- Show UserRole Page  ---------------
-
-    public function index( Request $request ) {
-
-        $user = User::all( 'name', 'id' );
-
-        // $data = DB::table( 'role' )->get();
-
-        return view( 'backend.admin.subadmin.addRole' )->with( [ 'user' => $user ] );
+    public function index(Request $request){
+        try{
+            $user = User::all('name', 'id');
+            // $data = DB::table( 'role' )->get();
+            return view('backend.admin.subadmin.addRole')->with(['user' => $user]);
+            return response()->json(['user' => $user]);
+        } catch (Exception $e) {
+            return response()->json('faild');
+        }
     }
     //*------------- Get Data In RoleTable ---------------
-
-    public function getUserRoles( Request $request, User $user ) {
-
-        $data = DB::table( 'role' )->get();
-
-        return response()->json( [ 'data' => $data ] );
+    public function getUserRoles(Request $request, User $user){
+        try{
+            $data = DB::table('role')->get();
+            return response()->json(['data' => $data]);
+        } catch (Exception $e) {
+            return response()->json('false');
+        }
     }
-
     //*------------- Insert Data from Roleform  ---------------
-
-    public function insertUserRole( Request $request ) {
+    public function insertUserRole(Request $request){
         try {
-            $result = DB::table( 'role' )->insert( [
-                'name' => $request->name,
-            ] );
-            return response()->json( [ 'role' => $result ], 200 );
-        } catch( Exception $e ) {
-            return response()->json( 'false', 200 );
+            $result = DB::table('role')->insert([
+                'name' => $request->name, ]);
+            return response()->json(['role' => $result]);
+        } catch (Exception $e) {
+            return response()->json('false');
+        }
+    }
+    //*---------- Edit Data From Roleform -----------
+    public function editUserRole(Request $request){
+        try {
+            $data  = DB::table('role')->where('id', $request->id)->get();
+            return response()->json(['data' => $data]);
+        } catch (Exception $e) {
+            return response()->json('false');
         }
     }
 
+    //*---------- Update Data From Roleform -----------
+    public function updateUserRole(Request $request){
+        try{
+            $userdata = DB::select( " UPDATE role SET name = '$request->name' where id = '$request->id' ");
+            return response()->json(['userdata' => $userdata]);
+        } catch (Exception $e) {
+            return response()->json('false');
+        }
+    }
 }
--- resources/views/backend/admin/subadmin/addRole.blade.php
+++ resources/views/backend/admin/subadmin/addRole.blade.php
@@ -1,53 +1,61 @@
 @extends('layouts.master')
 @section('section')
- 
- 
-<!-- Content Wrapper. Contains page content -->
-@yield('section')
-
-<!-- Main content -->
+    <!-- Content Wrapper. Contains page content -->
+    @yield('section')
+    <!-- Main content -->
     <section class="content"><br>
         <div class="container-fluid">
             <div class="container pt-3">
-                <h2>User Role</h2>
+                <h2>Role List</h2>
                 <!-- AddUser button -->
                 <div class="addfrom-btn mb-5">
-                    <button id="Mybtn" class="btn btn-outline-secondary float-right add-bttn">Add</button>
+                    <button id="Mybtn" class="btn btn-primary float-right add-bttn">Add</button>
                 </div>
-                <!--End AddUser button -->
                 <!-- User Table -->
-                <div class="table-responsive  mt-5 " id="animateDataTable">
-                    <table id="role-table" class="align-middle mb-0 table table-border  table-striped table-hover" cellspacing="0" width="100%">
-                        <thead>
-                            <tr>
-                                <th>Name</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        </tbody>
-                    </table>
+                <div class="  mt-5 " id="animateDataTable">
+                    <div class="card">
+                        <div class="card-body">
+                            <table id="role-table" class="align-middle mb-0 table table-border  " cellspacing="0"
+                                width="100%">
+                                <thead>
+                                    <tr>
+                                        <th>Id</th>
+                                        <th>Name</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
-                <!-- End User Table -->
-                <!-- User Form  -->
-                <div id="MyForm-data">
-                    <div class="sticky">
-                        <form id="userRole-data" action="">
-                            <div class="row ">
-                                <div class="col-sm-12 form-field ">
-                                    <input type="text" name="name" id="first-name" class="form-control"
-                                        placeholder="Enter First Name" autocomplete="off" />
-                                    <small></small>
-                                </div>
-                                <div class="confirm-button-style pt-4 ml-3">
-                                    <button type="submit" class="btn btn-outline-secondary confirm-button" name="submit"
-                                        id="btnSubmit">Submit</button>
+                <!-- User Form InsertData -->
+                <div id="MyForm-InsertData">
+                    <div class="sticky-form">
+                        <form method="POST" id="userRole-data">
+                            @csrf
+                            <div class="card card-primary">
+                                <div class="card-body">
+                                    <h3>Role List</h3>
+                                    <div class="form-group">
+                                        <input type="hidden" id="roleId">
+                                    </div>
+                                    <div class="form-group form-field">
+                                        <input type="text" name="name" id="first-name" class="form-control userName"
+                                            placeholder="Enter Name" autocomplete="off" />
+                                        <small></small>
+                                    </div>
+                                    <div class="card-footer">
+                                        <button type="submit" id="btnSubmit" class="btn btn-primary">Submit</button>
+                                        <button type="submit" id="back-btn"
+                                            class="btn btn-primary float-right">Back</button>
+                                    </div>
                                 </div>
                             </div>
                         </form>
                     </div>
                 </div>
-                <!-- End User Form  -->
             </div>
         </div>
-    </section>        
+    </section>
 @endsection
